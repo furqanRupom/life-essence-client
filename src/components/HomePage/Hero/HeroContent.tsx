d342a548c147c4591007d7442b1a346e82f113cc
furqanrupom <furqanrupom978@gmail.com>
2024-05-25 12:38:22 +0600

    added favion.ico file

--- src/components/HomePage/Hero/HeroContent.tsx
+++ src/components/HomePage/Hero/HeroContent.tsx
@@ -38,7 +38,7 @@ export const HeroContent: React.FunctionComponent<IHeroContentProps> = ({ title,
                         <div className="relative px-4 py-16 mx-auto overflow-hidden sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                         <div className={`flex flex-col items-center justify-between  ${isRowReverse ? "xl:flex-row-reverse" : "xl:flex-row"}`}>
                                 <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
-                                    <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-coral-400 sm:text-4xl sm:leading-none">
+                                    <h2 className="max-w-lg mb-6  text-3xl font-extrabold tracking-tight text-coral-400 sm:text-4xl sm:leading-none">
                                         {title}
                                     </h2>
                                     <p className="max-w-xl mb-4 text-base text-default-50 md:text-lg">

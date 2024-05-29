8499b1ce0e637f5b80455f154f47e9e55f59efb4
furqanrupom <furqanrupom978@gmail.com>
2024-05-29 23:56:28 +0600

    fixed some bugs

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,6 +2,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
     reducerPath: "baseApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://blood-donate-three.vercel.app/api" }),
     endpoints: () => ({})
 })
